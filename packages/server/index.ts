e3f95fdab757f1b20ea3f3293c69a9ec64afd477
Rahul Tevatia <rtevatia2@gmail.com>
2026-04-08 22:22:36 -0400

    chore(format): set up prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import type { Request, Response } from "express";
+import express from 'express';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,14 +8,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!');
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello world!"});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello world!' });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
